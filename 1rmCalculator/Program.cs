9fed5caad75a8d0ce15374f8d4627162ce0e70b5
Stoycho Kyurpeev <StoichoK@users.noreply.github.com>
2018-06-12 12:24:48 +0300

    Add files via upload

---
+++ 1rmCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1rmCalculator
+{
+    class OneRepMaxCalculator
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Изчисляване на тежестта за едно повторение в серия по формула Brzycki от Matt Brzycki.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Въведете работни кг. - ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Въведете колко повторения правите с тези кг. - ");
+            int reps = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Вашите максимални кг. за едно повторение са -");
+            var formulaResult = weight / (1.0278 - 0.0278 * reps);
+            Console.WriteLine($"{Math.Round(formulaResult)} кг.");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
